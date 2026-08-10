2628cee494157b8eb769b41f42bf6984e1d836ab
Jônathas Borges <jonathas.m.borges@aluno.senai.br>
2026-08-10 16:30:20 -0300

    Adiciona edição, exclusão e estilização dinâmica dos botões de ação

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-// Estrutura de dados para o cadastro de cidades
 interface CidadeEleitoral {
   nome: string;
   eleitores: number;
@@ -16,28 +15,55 @@ interface CidadeEleitoral {
   styleUrls: ['./app.css']
 })
 export class App {
-  // Propriedades vinculadas aos campos do formulário
   nomeCidade: string = '';
   quantidadeEleitores: number | null = null;
-  
-  // Lista para armazenamento das cidades cadastradas
   cidades: CidadeEleitoral[] = [];
 
-  // Método para validação e inclusão de nova cidade na lista
+  // Variável de controle para edição
+  indiceEmEdicao: number | null = null;
+
   adicionarCidade() {
     if (this.nomeCidade && this.quantidadeEleitores !== null) {
-      
-      this.cidades.push({
-        nome: this.nomeCidade,
-        eleitores: this.quantidadeEleitores
-      });
-      
-      // Reinicializa os campos do formulário
+
+      if (this.indiceEmEdicao !== null) {
+        // Atualiza a cidade existente
+        this.cidades[this.indiceEmEdicao] = {
+          nome: this.nomeCidade,
+          eleitores: this.quantidadeEleitores
+        };
+        this.indiceEmEdicao = null;
+      } else {
+        // Cadastra uma nova cidade
+        this.cidades.push({
+          nome: this.nomeCidade,
+          eleitores: this.quantidadeEleitores
+        });
+      }
+
+      // Limpa os campos
       this.nomeCidade = '';
       this.quantidadeEleitores = null;
-      
+
     } else {
       alert('Por favor, preencha o nome da cidade e a quantidade de eleitores.');
+    }
+  }
+
+  // Método chamado pelo botão Editar do HTML
+  editarCidade(index: number) {
+    this.nomeCidade = this.cidades[index].nome;
+    this.quantidadeEleitores = this.cidades[index].eleitores;
+    this.indiceEmEdicao = index;
+  }
+
+  // Método chamado pelo botão Excluir do HTML
+  excluirCidade(index: number) {
+    this.cidades.splice(index, 1);
+
+    if (this.indiceEmEdicao === index) {
+      this.indiceEmEdicao = null;
+      this.nomeCidade = '';
+      this.quantidadeEleitores = null;
     }
   }
 }
